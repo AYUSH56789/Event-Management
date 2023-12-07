12de2a9eb502f096c10c0a84e375c11e33060baa
Ayush Singh <ayushsingh46026@gmal.com>
2023-12-07 19:31:35 +0530

    add user-home page

--- resources/views/student/home.blade.php
+++ resources/views/student/home.blade.php
@@ -2,5 +2,85 @@
 @push('title')
 <title>student</title>
 @section('main-section')
-<h1>student-section is under maintainance</h1>
+
+<div class="container-fluid">
+    <div class="row py-2">
+        <div class="col-md-4 col-sm-12 ">
+            <div class="container-fluid  text-white py-2 rounded-top"
+        style="display: flex; justify-content: center; align-items: center;  background-color: black;">
+        <h4 class="m-0 p-0">Student Detail</h4>
+    </div>
+            <table class="table table-striped table-inverse table-responsive">
+                <tbody>
+                    <tr>
+                        <td scope="row">Name</td>
+                        <td>Ayush Singh</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Branch</td>
+                        <td>Information Technology</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Year</td>
+                        <td>3rd year</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Urn</td>
+                        <td>2104484</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Crn</td>
+                        <td>2121028</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Contact</td>
+                        <td>977994578</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Email</td>
+                        <td>ayushsingh123@gmail.com</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-8 col-sm-12 ">
+            <div class="container-fluid  text-white py-2 rounded-top"
+            style="display: flex; justify-content: center; align-items: center;  background-color: black;">
+            <h4 class="m-0 p-0">Registered Event</h4>
+        </div>
+            <table class="table  table-inverse table-responsive">
+                <thead class="thead-inverse">
+                    <tr>
+                        <th>Event</th>
+                        <th>Date</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>ayush singh</td>
+                        <td>ayush singh</td>
+                        <td class="d-flex align-item-center justify-content-center"><button type="reset" class="btn w-80  custom-btn py-0 ">Know More</button></td>
+                    </tr>
+                    <tr>
+                        <td>ayush singh</td>
+                        <td>ayush singh</td>
+                        <td class="d-flex align-item-center justify-content-center"><button type="reset" class="btn w-80  custom-btn py-0 ">Know More</button></td>
+                    </tr>
+                    <tr>
+                        <td>ayush singh</td>
+                        <td>ayush singh</td>
+                        <td class="d-flex align-item-center justify-content-center"><button type="reset" class="btn w-80  custom-btn py-0 ">Know More</button></td>
+                    </tr>
+                    <tr>
+                        <td>ayush singh</td>
+                        <td>ayush singh</td>
+                        <td class="d-flex align-item-center justify-content-center"><button type="reset" class="btn w-80  custom-btn py-0 ">Know More</button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
 @endsection
