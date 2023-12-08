cfec15677fac23fec3e9ea425c78dfbd31521647
Ayush Singh <ayushsingh46026@gmal.com>
2023-12-08 17:16:47 +0530

    complete certifate / event / attendence section

--- resources/views/student/attendence.blade.php
+++ resources/views/student/attendence.blade.php
@@ -2,5 +2,8 @@
 @push('title')
 <title>student-attendence</title>
 @section('main-section')
-<h1>student-attendence section is under maintainance</h1>
+{{-- <h1>student-attendence section is under maintainance</h1> --}}
+<div class="container text-center">
+    <p class="text-danger m-2">You Are Either not Register for any Event Or Attendence is not activate by Event Management!</p>
+</div>
 @endsection
--- resources/views/student/certificate.blade.php
+++ resources/views/student/certificate.blade.php
@@ -2,5 +2,29 @@
 @push('title')
 <title>student-certificate</title>
 @section('main-section')
-<h1>student-certificate section is under maintainance</h1>
+{{-- <h1>student-certificate section is under maintainance</h1> --}}
+<form action="" method="post">
+    <!-- @csrf -->
+    <div class="row px-4">
+        <div class="col  py-2 pt-3">
+            <select class="form-select form-select" name="society" aria-label=".form-select-lg example">
+                <option selected>Select Event</option>
+                <option value="CSI">Hacking</option>
+                <option value="ISTE">Cryptocruncy</option>
+                <option value="ITian club">AI/ML</option>
+              </select>
+              <span class="text-danger">
+                @error('society')
+                {{$message}}
+                @enderror
+              </span> 
+        </div>
+    </div>
+    <div class="row">
+      <div class="container text-center my-2 ">
+        <button type="submit" class="btn custom-btn  mx-3 w-50">Get Certificate</button>
+        {{-- <button type="reset" class="  custom-btn mx-3 p-1 w-25">Reset</button> --}}
+    </div>
+    </div>
+    </form>
 @endsection
